e2c37ab3c6243ff8a0a8b116cb3b0a077e19d7eb
saifalisaiyed <saifalisaiyed49@gmail.com>
2019-03-02 19:09:56 -0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,89 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        int i = -4;
+
+        while (i < 5) {
+
+            System.out.println("Counting... " + i);
+            i++;
+        }
+
+
+
+
+
+//        for (int i = 0; i < 20; i++) {
+//
+//            if (i % 4 == 0) {
+//
+//                System.out.println( i + " is a multiple of 4 ");
+//
+//            }
+//
+//        }
+
+    }
+
+}
+
+
+
+
+
+//	 write your code here
+//        Addition = +
+//        Subtraction "-"
+//        Multiplication "*"
+//        Division "/"
+//        Remainder = "What remains.." 4 % 2 = 0
+//         4 % 3 = 1
+//
+//         === Relational Operators ===
+//
+//           - == comparison, not assignment
+//           - != NOT equal
+//           - > greater than
+//           - < less than
+//           - >= Greater than or equal
+//           - <= Less than or equal
+//
+//           === Logical Operators ===
+//            - AND ( && ) == both sides need to be true for the whole expression to return true
+//            - OR ( || )
+//            - NOT ( ! )
+//
+//
+//        double firstNum = 10;
+//        double secondNum = 3;
+//        double result = 0;
+//
+//        boolean isOld = true;
+//        boolean isYoung = false;
+//
+//
+//        if ( (isOld && !isYoung) && (firstNum < secondNum) ) {
+//            System.out.println("Hooray!");
+//        }else {
+//            System.out.println("Not right!");
+//        }
+//
+//        result = firstNum % secondNum;
+//
+//        // If statements
+//        if (firstNum <= secondNum) {
+//
+//            System.out.println("The remainder is " + result );
+//
+//        }else {
+//
+//            System.out.println("Numbers are not equal");
+//        }
+//
+//
+//    }
+//}
